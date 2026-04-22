1429d6e01f50a316f3892e59db8ec9c35c08b218
emfo2405 <emfo2405@student.miun.se>
2026-04-22 14:36:13 +0200

    Interface skapat för startsida

---
+++ types/wordpress.types.ts
@@ -0,0 +1,25 @@
+//Struktur för en startsida
+export interface Startsida {
+    id: number;
+    slug: string;
+    title: {
+        rendered: string;
+    };
+    content: {
+        rendered: string;
+    };
+    excerpt: {
+        rendered: string;
+    };
+    acf: {
+        sidtitel: string;
+        sid_tagline: string;
+        om_oss_titel: string;
+        om_oss_text: string;
+        nasta_event_titel: string;
+        senaste_produkter_titel: string;
+        kraftolskamrat_tagline: string;
+        kraftolskamrat_lanktext: string;
+        omoss_bild: string;
+    };
+}
