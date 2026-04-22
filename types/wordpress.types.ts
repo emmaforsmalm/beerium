4843dd4b6d5a02f6fdfa71f2f6c63ca3568c0af8
emfo2405 <emfo2405@student.miun.se>
2026-04-22 15:05:43 +0200

    Interface tillagt för kalender, om oss och medlem

--- types/wordpress.types.ts
+++ types/wordpress.types.ts
@@ -46,3 +46,68 @@ export interface Sortiment {
         kontakt_info: string;
     };
 }
+
+//Struktur för kalendersidan
+export interface Kalender {
+    id: number;
+    slug: string;
+    title: {
+        rendered: string;
+    };
+    content: {
+        rendered: string;
+    };
+    excerpt: {
+        rendered: string;
+    };
+    acf: {
+        kalender_titel: string;
+        bild_titel: string;
+    };
+}
+
+//Struktur för om osssidan
+export interface OmOss {
+    id: number;
+    slug: string;
+    title: {
+        rendered: string;
+    };
+    content: {
+        rendered: string;
+    };
+    excerpt: {
+        rendered: string;
+    };
+    acf: {
+        om_oss_titel: string;
+        om_oss_text: string;
+        kontakt_titel: string;
+        kontakt_text: string;
+        adress_titel: string;
+        adress_text: string;
+        epost_titel: string;
+        email_text: string;
+    };
+}
+
+//Struktur för medlemssidan
+export interface Medlem {
+    id: number;
+    slug: string;
+    title: {
+        rendered: string;
+    };
+    content: {
+        rendered: string;
+    };
+    excerpt: {
+        rendered: string;
+    };
+    acf: {
+        medlem_sidtitel: string;
+        medlem_tagline: string;
+        medlem_text: string;
+    };
+}
+
