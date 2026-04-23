853c6a8e4ce57e47d85eaaff7944499d6cc29314
emfo2405 <emfo2405@student.miun.se>
2026-04-23 13:31:50 +0200

    bild inläst till header på startsidan

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
 
 //Importera funktion för att läsa in startsida från wordpress
-import { getStartPage } from "@/apiReq/wordpressApi";
+import { getStartPage, getMedia } from "@/apiReq/wordpressApi";
 
 export default async function Home() {
 
   const page = await getStartPage();
 
+  let imgUrlHeader = "brewery.jpg";
+
+  if (page.acf.header_bild) {
+      imgUrlHeader = await getMedia(page.acf.header_bild);
+  }
+
+
   return (
     <div>
       <main>
-        <h1>{page.acf.sidtitel}</h1>
-        <p>{page.acf.sid_tagline}</p>
+        <div>
+          <img src={imgUrlHeader}></img>
+          <h1>{page.acf.sidtitel}</h1>
+          <p>{page.acf.sid_tagline}</p>
+        </div>
+
       </main>
     </div>
   );
