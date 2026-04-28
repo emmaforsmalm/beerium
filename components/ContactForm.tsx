b0c07316f165032a57b8c481a9cd895feb1748d9
emfo2405 <emfo2405@student.miun.se>
2026-04-28 11:20:09 +0200

    Komponent för att hantera kontaktformulär skapad

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useState } from "react"
+import { useState } from "react";
+import { sendContactForm } from "@/apiReq/wordpressApi";
 
 export default function contactForm () {
 
@@ -8,34 +9,44 @@ export default function contactForm () {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    const [loading, setLoading] = useState("");
-    const [confirmation, setConfirmation] = useState("");
-    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [confirmation, setConfirmation] = useState(false);
+    const [error, setError] = useState(false);
 
     //Funktion för att skicka in ett formulär
     const handleSubmit = async(event:any) => {
         event.preventDefault();
+        setLoading(true);
 
-        //Sätt error till tomt
-        setError("");
+        try {
+            await sendContactForm(name, email, message);
+            setConfirmation(true);
+            setLoading(false);
+        }catch(err) {
+            setError(true)
+            setLoading(false);
+        }
 
-        
     }
 
     return (
         
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label htmlFor="name">Namn:</label><br/>
-            <input type="text" id="name" required></input><br/>
+            <input type="text" id="name" required value={name} onChange={(e) => setName(e.target.value)}></input><br/>
 
             <label htmlFor="email">E-post:</label><br/>
-            <input type="email" id="email" required></input><br/>
+            <input type="email" id="email" required value={email} onChange={(e) => setEmail(e.target.value)}></input><br/>
 
             <label htmlFor="message">Meddelande:</label><br/>
-            <input type="textarea" id="message" required></input><br/>
+            <input type="textarea" id="message" required value={message} onChange={(e) => setMessage(e.target.value)}></input><br/>
 
             <input type="submit" value="Skicka"></input>
+
+            {loading && (
+                <p>Laddar...</p>
+            )}
           </form>
 
         </div>
