bc07059757d34bde0b1f12cbcd510764ebaf0f89
emfo2405 <emfo2405@student.miun.se>
2026-05-29 21:24:19 +0200

    Tillgänglighetsanpassningar gjorda

--- app/betalning/page.tsx
+++ app/betalning/page.tsx
@@ -1,6 +1,11 @@
+export const metadata: Metadata = {
+  title: "Betalningssida - Beerium",
+  description: "Betalningssida för medlemskap i Beerium"
+}
 
 import { getPaymentInfo } from "@/apiReq/wordpressApi";
 import Betalning from "./BetalningClient";
+import { Metadata } from "next";
 
 export default async function Payment() {
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -62,9 +62,11 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
       <body>
+        <main>
         <NavComponent />
         {children}
         <FooterComponent/>
+        </main>
       </body>
     </html>
   );
